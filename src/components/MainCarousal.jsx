56027bf615c4a536821e880079db17f16f274137
unknown <sharinhansha@gmail.com>
2025-08-15 16:54:02 +0530

    basic

---
+++ src/components/MainCarousal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainCarousal = () => {
+  return (
+    <div>MainCarousal</div>
+  )
+}
+
+export default MainCarousal
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './styles/global.css'
+import UserLayout from './layout/UserLayout';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+// import { store } from './globalState/store'
+// import { Provider } from 'react-redux'
+
+const router = createBrowserRouter([
+  {
+    path:'',
+    element:<UserLayout/>,
+   children:[{ 
+    path: "/",
+    element: <HomePage/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },]
+}
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+              <RouterProvider router={router} />
+
+    {/* </Provider>, */}
   </StrictMode>,
 )
